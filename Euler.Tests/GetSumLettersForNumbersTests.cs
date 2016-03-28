9ac8297d294b510d5254c1dd26f95eedcffad49a
Robert Sieber <rssiebersdata@gmail.com>
2016-03-28 09:54:24 -0600

    addition of final comments

--- Euler.Tests/GetSumLettersForNumbersTests.cs
+++ Euler.Tests/GetSumLettersForNumbersTests.cs
@@ -4,6 +4,7 @@ using EulerT;
 
 namespace Euler.Tests
 {
+    //Test verifies sum of all letters for numbers 1 to 1000
     [TestClass]
     public class GetSumLettersForNumbersTests
     {
@@ -11,9 +12,12 @@ namespace Euler.Tests
         public void GetSumLettersForNumbers_Test()
         {
 
-            int res = 0;
+            int res = 0; //setup result var
+
+            //run test in Euler Utility class directly
             res = Utility.GetSumLettersForNumbers();
 
+            //Verify result is as expected
             if (res == 21124)
             {
                 Assert.IsTrue(true);
