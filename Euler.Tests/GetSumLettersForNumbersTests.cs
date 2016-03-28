3bc75ade87183b889d31e29fdd380f88565d3796
Robert Sieber <rssiebersdata@gmail.com>
2016-03-27 22:36:17 -0600

    Added GetSumLettersForNumbers test to validate final result.

---
+++ Euler.Tests/GetSumLettersForNumbersTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EulerT;
+
+namespace Euler.Tests
+{
+    [TestClass]
+    public class GetSumLettersForNumbersTests
+    {
+        [TestMethod]
+        public void GetSumLettersForNumbers_Test()
+        {
+
+            int res = 0;
+            res = Utility.GetSumLettersForNumbers();
+
+            if (res == 21124)
+            {
+                Assert.IsTrue(true);
+            }
+            else
+            {
+                Assert.IsTrue(false);
+            }
+        }
+    }
+}
