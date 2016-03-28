cd7ec33c4d0668e11e65f2fec71f67d620195f2c
Robert Sieber <rssiebersdata@gmail.com>
2016-03-27 21:06:09 -0600

    Added multiples functions and utility class for working functions

--- Program.cs
+++ Program.cs
@@ -13,14 +13,13 @@ namespace EulerT
 
             var runSync = Task.Factory.StartNew(new Func<Task>(async () =>
             {
-                int call = await Task.FromResult<int>(1);
-                Console.WriteLine("Multiples Answer: " + call.ToString());
-                int call2 = await Task.FromResult<int>(2);
-                Console.WriteLine("Number of letters in 1 to 1000 Answer: " + call2.ToString());
+                int call = await Task.FromResult<int>(Utility.GetSumOfMultiples());
+                Console.WriteLine("Find the sum of all the multiples of 3 or 5 below 1000 - ANSWER: " + call.ToString());
+                //int call2 = await Task.FromResult<int>(2);
+                //Console.WriteLine("Number of letters in 1 to 1000 Answer: " + call2.ToString());
 
             })).Unwrap();
 
-            Console.WriteLine("Find the sum of all the multiples of 3 or 5 below 1000");
             Console.WriteLine("Task Started");
             runSync.Wait();
             Console.WriteLine("Task Completed, Press Esc Key To End....");
---
+++ Utility.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerT
+{
+    class Utility
+    {
+        //This function is designed to find
+        //the sum of all the multiples of 3 or 5 below 1000
+        //and return the answer.
+        public static int GetSumOfMultiples()
+        {
+            int res = 0; //setup result var
+            int res3 = 0; //multiples of 3 result
+            int res5 = 0; //multiples of 5 result
+            int res15 = 0; //multiples of 15 result
+
+            //trap any unexpected errors
+            try
+            {
+                //get multiples of 3 for 1000
+                res3 = getmultiplesof(3, 1000);
+                //get multiples of 5 for 1000
+                res5 = getmultiplesof(5, 1000);
+                //get multipes of 15 for 1000
+                res15 = getmultiplesof(15, 1000);
+
+
+                //combine multiples of 3 and 5, and subtract multiples of 15
+                //because these are duplicate multiples
+                res = (res3 + res5) - res15;
+
+                //return result
+                return res;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public static int getmultiplesof(int dby, int howmany)
+        {
+            int res = 0; //setup return var
+
+            //trap any unexpected errors
+            try
+            {
+
+                //loop through 1000 and find the multiples of dby
+                for (int i = 1; i < howmany; i++)
+            {
+                //determine remainder, if none, then it is a multiple
+                if (i % dby == 0)
+                {
+                    //add multiple to resule
+                    res += i;
+                }
+            }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            //return final result
+            return res;
+        }
+    }
+}
